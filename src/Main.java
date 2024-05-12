d43299113de617ce570fe61abc1299cfffabe9c8
Matěj K <hobitekCZ@post.cz>
2024-05-12 23:40:42 +0200

    # Competition of assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i < 100; i++) {
+            StringBuilder outcome = new StringBuilder();
+            if (i % 2 == 0) {
+                outcome.append("Baz");
+            }
+            if (i % 5 == 0) {
+                outcome.append("inga!");
+            }
+            if (outcome.isEmpty()) {
+                System.out.println(i);
+            } else {
+                System.out.println(outcome);
+            }
+        }
     }
 }
